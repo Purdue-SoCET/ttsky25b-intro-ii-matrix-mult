674a62e9724f37b6173b1b86e7c4e8708519b288
Miguel Isrrael Teran <misrrael@purdue.edu>
2025-10-27 23:04:24 -0400

    Fill out YAML file, project top-level source file, and config.json

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_intro_ii_matrix_mult (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,23 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[7:3]  = '0;  // Unused dedicated outputs
+  assign uio_out = '0;  // Not using bidirectional output path
+  assign uio_oe  = '0;  // Will use ALL bidirectional pins as inputs
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+
+  // Instantiate Project
+  matrix_mult matrix_mult_proj (
+    .clk(clk),
+    .n_rst(rst_n),
+    .confirmation(ui_in[0]),
+    .serial_in(ui_in[1]),
+    .bit_period({uio_in, ui_in[7:2]}),
+    .data_read(uo_out[0]),
+    .serial_out(uo_out[1]),
+    .tx_busy(uo_out[2])
+  );
 
 endmodule
